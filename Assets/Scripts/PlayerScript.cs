558f371d814044753fd79b39f92f743ca10c963e
unknown <rook49900@gmail.com>
2020-10-22 01:57:34 -0400

    First commit

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -2,22 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
- 
+
 public class PlayerScript : MonoBehaviour
 {
     private Rigidbody2D rd2d;
- 
-    public float speed;
- 
+    public float speed = 15;
     public Text score;
- 
+    public Text winText;
+    public Text livesText;
     private int scoreValue = 0;
+    private int lives;
+    public GameObject player;
  
     // Start is called before the first frame update
     void Start()
     {
         rd2d = GetComponent<Rigidbody2D>();
-        score.text = scoreValue.ToString();
+        score.text = "Score: " + scoreValue.ToString();
+        winText.text = "";
+        lives = 3;
+        livesText.text = "Lives: " + lives.ToString();
     }
  
     // I had a ton of trouble here but things finally work, thank god
@@ -33,7 +37,15 @@ public class PlayerScript : MonoBehaviour
        if (collision.collider.tag == "Coin")
         {
             scoreValue += 1;
-            score.text = scoreValue.ToString();
+            score.text = "Score: " + scoreValue.ToString();
+            Destroy(collision.collider.gameObject);
+            SetScoreText ();
+        }
+
+        if (collision.collider.tag == "Enemy")
+        {
+            lives -= 1;
+            SetLivesText ();
             Destroy(collision.collider.gameObject);
         }
  
@@ -54,5 +66,26 @@ public class PlayerScript : MonoBehaviour
             }
         }
     }
+
+    private void SetScoreText ()
+    {
+        if (scoreValue == 4)
+        {
+            winText.text = "You win! Game created by Chase Rook.";
+        }
+    
+    }
+
+      void SetLivesText ()
+    {
+        livesText.text = "Lives: " + lives.ToString ();
+        if (lives <= 0)
+        {
+            Destroy(player);
+            winText.text = "You lose! Game created by Chase Rook.";
+        }
+    }
 }
+
+
 
